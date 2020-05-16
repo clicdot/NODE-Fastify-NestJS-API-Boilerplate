b43a7be1eb3d1121d0c942e303d5bbc5c8f72ba8
Philip Choi <philip.choi@gmail.com>
2020-05-16 10:06:25 -0500

    unit test fix

--- src/common/services/response/response.service.spec.ts
+++ src/common/services/response/response.service.spec.ts
@@ -15,4 +15,43 @@ describe('ResponseService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Reply ', () => {
+    it('Should get data', async () => {
+      const type = 1;
+      expect(await service.set(type)).toBe(type);
+    });
+
+    it('Reply type 1', async () => {
+      const type = 1;
+      expect(await service.set(type)).toBe(type);
+      const reply = {
+        code (status) {
+          return this;
+        },
+        send (json) {
+          return this;
+        },
+      };
+      const expectedResult = {};
+      const opt = {};
+
+      expect(await service.reply(reply, opt)).toMatchObject(expectedResult);
+    });
+
+    it('Reply type 2', async () => {
+      const reply = {
+        status (status) {
+          return this;
+        },
+        json (json) {
+          return this;
+        },
+      };
+      const expectedResult = {};
+      const opt = {};
+
+      expect(await service.reply(reply, opt)).toMatchObject(expectedResult);
+    });
+  });
 });
