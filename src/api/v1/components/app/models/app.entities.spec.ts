7bb62617db9de0717ac80a3c3ecace295bdd9f51
Philip Choi <philip@clicdot.com>
2021-10-03 19:53:38 -0500

    unit test fix

--- src/api/v1/components/app/models/app.entities.spec.ts
+++ src/api/v1/components/app/models/app.entities.spec.ts
@@ -2,17 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { App } from './app.entities';
 
 describe('App Entities', () => {
-  let app: App;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: []
-    }).compile();
-
-    app = module.get<App>(App);
-  });
-
   it('App - should be defined', () => {
-    expect(app).toBeDefined();
+    expect(App).toBeDefined();
   });
 });
